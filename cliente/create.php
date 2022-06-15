54a9d3efa4ffc9c7117d3fae0583dfd9089e361e
Douglas <rayolpereira2@gmail.com>
2022-06-14 21:28:14 -0300

    Initial commit

---
+++ cliente/create.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+unset($_SESSION['msg']);
+require_once "../util/config.php";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $nome = $_POST["nome"];
+  $telefone = $_POST["telefone"];
+  $endereco = $_POST["endereco"];
+  $senha = $_POST["senha"];
+  $email = $_POST["email"];
+  $cpf = $_POST["cpf"];
+  $cidade = $_POST["cidade"];
+  $estado = $_POST["estado"];
+  $cep = $_POST["cep"];
+  $nascimento = $_POST["nascimento"];
+
+  $sql = "INSERT INTO usuario (nome, telefone, endereco, senha, email, cpf, cidade, estado, cep, nascimento) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+  $stmt = mysqli_prepare($link, $sql);
+
+  mysqli_stmt_bind_param($stmt, "ssssssssss", $nome, $telefone, $endereco, $senha, $email, $cpf, $cidade, $estado, $cep, $nascimento);
+
+  if (mysqli_stmt_execute($stmt)) {
+    $_SESSION['msg'] = "<center>Cadastro com sucesso";
+  } else {
+    $_SESSION['msg'] = "Erro no cadastro";
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Upet</title>
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+  <div id="lista">
+    <div class="div1">
+      <img src="../css/Cat.png" id="gato">
+    </div>
+
+    <div class="div2">
+      <h1 class="texto-cadastro">Cadastro</h1>
+      <h2 class="texto-cadastro">Crie sua conta</h2>
+      <form method="POST" action="create.php" class="formulario">
+        <div class="form-row">
+          <div class="nomecadastro">
+            <input type="text" class="input-cadastro" name="nome" placeholder="Nome" required>
+          </div>
+          <br>
+
+          <div class="col-md-4 mb-3">
+            <input type="text" class="input-cadastro" name="cpf" placeholder="CPF" required>
+          </div>
+          <br>
+
+          <div class="input-group input-group-sm mb-3">
+            <input type="text" class="input-cadastro" name="telefone" placeholder="Telefone" required>
+          </div>
+          <br>
+
+          <div class="col-md-4 mb-3">
+            <input type="date" class="input-cadastro" name="nascimento" placeholder="Nascimento (DD/MM/AAAA)" required>
+          </div>
+          <br>
+
+          <div class="input-group input-group-sm mb-3">
+            <input type="text" class="input-cadastro" name="email" placeholder="Email" required>
+          </div>
+          <br>
+
+          <div class="form-row">
+            <div class="cidadecadastro">
+              <input type="text" class="input-cadastro" name="cidade" placeholder="Cidade" required>
+            </div>
+            <br>
+
+            <div class="col-md-3 mb-3">
+              <input type="text" class="input-cadastro" name="estado" placeholder="Estado" required>
+            </div>
+            <br>
+
+            <div class="col-md-3 mb-3">
+              <input type="text" class="input-cadastro" name="cep" placeholder="CEP" required>
+            </div>
+            <br>
+            <div>
+              <input type="text" class="input-cadastro" name="endereco" placeholder="Endereço" required>
+            </div>
+            <br>
+            <div class="col-md-3 mb-3">
+              <input type="password" class="input-cadastro" name="senha" placeholder="Senha" required>
+            </div>
+            <br>
+            <div>
+              <input type="password" class="input-cadastro" name="confsenha" placeholder="Confirmar Senha" required>
+            </div>
+            <br>
+          </div>
+          <br>
+
+          <div class="input-cadastro" id="genero-cadastro"><strong>Gênero</strong>
+            <input type="radio" name="sexo" value="M"> Masculino
+            <input type="radio" name="sexo" value="F"> Feminino
+            <input type="radio" name="sexo" value="O"> Outros
+          </div>
+
+          <p><button type="submit" class="botao-cadastro">Cadastrar</button></p>
+
+          <p id="text-center">Já tem uma conta? <a href="../login/create.php">Login</a></p>
+
+          <?php
+          if (isset($_SESSION['msg']) == true) {
+            echo "<div class='input-cadastro'>";
+            echo $_SESSION['msg'];
+            echo "</div>";
+          }
+          ?>
+      </form>
+    </div>
+  </div>
+</body>
---
+++ login/login.php
@@ -0,0 +1,56 @@
+<?php
+    session_start();
+    unset($_SESSION['msg']);
+    require_once "../util/config.php";
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+
+        $sql = "SELECT * FROM usuario WHERE email = ? AND senha = ?";
+        $stmt = mysqli_prepare($link, $sql);
+        mysqli_stmt_bind_param($stmt, "ss", $email, $senha);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $row = mysqli_fetch_array($result);
+        if(mysqli_num_rows($result) > 0){
+            header('location: ../vitrine/index.php');
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['senha'] = $row['senha'];
+        }else{
+            $_SESSION['msg'] = "<center>Usuario ou senha inválido";
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de login</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <form method="POST">
+    <div id="login">
+        <h1 class="texto-login">Login</h1>
+        <h2 class="texto-login">Acesse sua conta</h2>
+        <input type="text" name="email" placeholder="Nome do usuário" class="input-login">
+        <br><br>
+        <input type="password" name="senha" placeholder="Senha" class="input-login">
+        <br><br>
+        <button type="submit" id="botao-login">Entrar</button><br><br>
+        <?php
+          if (isset($_SESSION['msg']) == true) {
+            echo "<div class='input-login'>";
+            echo $_SESSION['msg'];
+            echo "</div>";
+          }
+          ?>
+        <br>
+        <p class="texto-login">Não tem uma conta? <a href="../cliente/create.php">Cadastre-se</a></p>
+    </div>
+    <img src="../css/Dog.png" id="cachorro">
+    </form>
+</body>
+</html>
